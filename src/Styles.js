b6b407712fdc8259f03c147aa16778c3cfd82394
juliag4 <159590870+juliag4@users.noreply.github.com>
2026-04-23 20:15:34 -0400

    Update Styles.js

--- src/Styles.js
+++ src/Styles.js
@@ -1,9 +1,5 @@
 export const AddStyle = (css) => {
-    //console.log('1');
     const style = document.createElement('style');
-    //console.log('2');
-    //style.textContent = css;
-    //console.log('3');
-    //document.head.appendChild(style);
-    //console.log('4');
+    style.textContent = css;
+    document.head.appendChild(style);
 };
