6e9dd977b8a1597d6c4b834ffa6c036cbab892d6
juliag4 <julgum@proton.me>
2026-04-28 20:28:08 -0400

    refactored a little and added comments to Game.js

--- src/Game.js
+++ src/Game.js
@@ -35,14 +35,16 @@ class Game extends HTMLElement{
         this.mousePos = {x: 45, y: 230};
         this.circlePos = {x: 45, y: 230};
         
+        // Changes mouse position based on mousemove event
         this.canvas.addEventListener('mousemove', (e) => {
             this.mousePos.x = e.clientX;
             this.mousePos.y = e.clientY;
         });
-        this.redraw = this.redraw.bind(this);
+        
+        // Redraws the circle every 1000 seconds if the player does not run into the map boundaries
         const drawInterval = setInterval(() => {
-            const playerAlive = this.circlePos.x <= 460 && this.circlePos.x >= 40 &&
-                            this.circlePos.y <= 460 && this.circlePos.y >= 40;
+            const playerAlive = this.circlePos.x <= 1460 && this.circlePos.x >= 40 &&
+                            this.circlePos.y <= 1460 && this.circlePos.y >= 40;
             if(playerAlive){ this.redraw(); }
             else{
                 clearInterval(drawInterval);
@@ -52,26 +54,37 @@ class Game extends HTMLElement{
     };
     
     redraw(){
+        // Return if positions are the same
         if(this.circlePos.x === this.mousePos.x && this.circlePos.y === this.mousePos.y){
             return;
         }
         
-        this.ctx.clearRect(this.circlePos.x - 22, this.circlePos.y - 22, 44, 44);
+        const radius = 20;
+        const borderWidth = 2;
+        
+        // Clears the circle
+        // Subtracts from the center of the circle and erases based
+        // on its total height/width
+        this.ctx.clearRect(this.circlePos.x - radius - borderWidth, this.circlePos.y - radius - borderWidth, (radius * 2) + (borderWidth * 2), (radius * 2) + (borderWidth * 2));
 
+        // Calculates how far to move the circle
         const xDiff = this.mousePos.x - this.circlePos.x;
         const yDiff = this.mousePos.y - this.circlePos.y;
         
-        const changeX = xDiff <= 0 ? Math.max(xDiff, -20) : Math.min(xDiff, 20);
-        const changeY = yDiff <= 0 ? Math.max(yDiff, -20) : Math.min(yDiff, 20);
+        const maxSpeed = 20;
+        
+        const changeX = xDiff <= 0 ? Math.max(xDiff, -maxSpeed) : Math.min(xDiff, maxSpeed);
+        const changeY = yDiff <= 0 ? Math.max(yDiff, -maxSpeed) : Math.min(yDiff, maxSpeed);
         
         this.circlePos.x += changeX;
         this.circlePos.y += changeY;
-         
+        
+        // Styling of the circle itself
         this.ctx.beginPath();
-        this.ctx.arc(this.circlePos.x, this.circlePos.y, 20, 0, 2 * Math.PI);
+        this.ctx.arc(this.circlePos.x, this.circlePos.y, radius, 0, 2 * Math.PI);
         this.ctx.fillStyle = 'purple';
         this.ctx.fill();
-        this.ctx.lineWidth = 2;
+        this.ctx.lineWidth = borderWidth;
         this.ctx.strokeStyle = 'black';
         this.ctx.stroke();
         this.ctx.closePath();
